6f94878b2b57166d1843e8265e3de83f60bb8099
Craiky <31390334+Craiky@users.noreply.github.com>
2024-08-22 14:47:45 -0700

    Fixed Mess

--- Main Build/src.js
+++ Main Build/src.js
@@ -11,38 +11,91 @@ let gfx = canv.getContext("2d")
 //variables and whatnot
 
 let frictionCoefficient = 0.5;
+
+
 //Scale factor
 let sF = 1
 
+//keep track of clicking
 let click = 0
 
-let mylatesttap;
+
+
+
+
 
 //resizing Canvas
 window.onresize = () => {
     //not sure what is v
-
+    canvas.width = canvas.height = window.innerHeight > window.innerWidth ? window.innerWidth : window.innerHeight;
     //normalize everthing to a 700x700 game size
-    sF = canvas.width/700
+    sF = canvas.width/700;
 }
 
 //set up & stuff
 window.onload = () => {
-    canv.width = window.innerWidth;
-    canv.height = window.innerHeight;
+    canv.width = window.innerWidth
+    canv.height = window.innerHeight*10
+    onresize()
 
-    gameLoop();
+    document.title = "Loading..."
 
-    window.addEventListener("keyup",keyUp);
-    window.addEventListener("keydown",keyDown);
+    //createSpriteSheet()
+
+    gameLoop()
+    document.title = "Cleansing Taylor"
 }
 
 //game loop
 
 function gameLoop() {
+    ctx.save()
+    ctx.scale(sF,sF)
+
+    updateGame()
+    drawGame()
+
+    ctx.restore()
 
     window.requestAnimationFrame(gameLoop); //why not use an interval instead?
 }
+
+function updateGame(){
+
+}
+
+function drawGame(){
+
+}
+
+
+
+canvas.addEventListener("mousedown",inputStart)
+canvas.addEventListener("mouseup",inputEnd)
+
+window.addEventListener("keydown",keyDown)
+window.addEventListener("keyup",keyUp)
+
+
+
+function inputEnd(){
+    click = 0;
+}
+
+function inputStart(e){
+    e.preventDefault()
+
+    if(e.which == 1 || e.which == 0){
+        click = {
+            x:(e.clientX-v.getBoundingClientRect().left)/sF,
+            y:(e.clientY-v.getBoundingClientRect()).top/sF
+        };
+
+        var now = new Date().getTime();
+    }
+}
+
+
 //Keybind Functions
 
 //controls for the game (desktop)
@@ -95,9 +148,13 @@ function keyUp(evt) {
         console.log('interact');
     }
 }
-function onClick (evt) {
-    
-}
+
+
+
+
+
+
+
 
 // objects and classes
 let player = {

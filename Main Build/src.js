da74093a127ac540659e3587756f29d841566b69
Craiky <31390334+Craiky@users.noreply.github.com>
2024-08-22 14:34:49 -0700

    Finished Basic Set up

--- Main Build/src.js
+++ Main Build/src.js
@@ -4,35 +4,124 @@ let canvas = document.getElementById("c")
 let ctx = canvas.getContext("2d")
 
 //Buffer Canvas
-//Why do we need a buffer and a game canvas?
 let canv = document.getElementById("g")
 let gfx = canv.getContext("2d")
 
+//Scale factor
+let sF = 1
+
+let click = 0
+
+let mylatesttap;
+
 //resizing Canvas
 window.onresize = () => {
-    //not sure what is v
+    //set canvas width and height
+    canvas.width = canvas.height = window.innerHeight > window.innerWidth ? window.innerWidth : window.innerHeight
 
-    // v.style.width = v.style.height = canvas.width = canvas.height = window.innerHeight > window.innerWidth ? window.innerWidth : window.innerHeight
-    // sF = canvas.width/700
+    //normalize everthing to a 700x700 game size
+    sF = canvas.width/700
 }
 
 //set up & stuff
 window.onload = () => {
-    canv.width = window.innerWidth;
-    canv.height = window.innerHeight;
+    canv.width = window.innerWidth
+    canv.height = window.innerHeight*10
 
-    gameLoop();
+    onresize()
 
-    window.addEventListener("keyup",keyUp);
-    window.addEventListener("keydown",keyDown);
+    document.title = "Loading..."
+
+    //createSpriteSheet()
+
+    gameLoop()
+    document.title = "Cleansing Taylor"
+}
+
+
+//Game Loop
+function gameLoop() {
+
+    canvas.width ^= 0
+
+    ctx.save()
+    ctx.scale(sF,sF)
+
+    updateGame()
+    drawGame()
+
+    ctx.restore()
+
+    window.requestAnimationFrame(gameLoop);
+}
+
+
+//Update Loop
+function updateGame(){
+
+}
+
+//Draw Loop
+function drawGame(){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+canvas.addEventListener("mousedown",inputStart)
+canvas.addEventListener("mouseup",inputEnd)
+
+window.addEventListener("keydown",keyDown)
+window.addEventListener("keyup",keyUp)
+
+
+function inputEnd(){
+    click = 0;
+}
+
+function inputStart(e){
+    e.preventDefault()
+
+    if(e.which == 1 || e.which == 0){
+        click = {
+        x:(e.clientX-v.getBoundingClientRect().left)/sF,
+        y:(e.clientY-v.getBoundingClientRect().top)/sF
+        };
+
+        var now = new Date().getTime();
+
+        //var timesince = now - mylatesttap;
+
+        //mylatesttap = new Date().getTime();
+    }
 }
 
 //game loop
 
-function gameLoop() {
 
-    window.requestAnimationFrame(gameLoop); //why not use an interval instead?
-}
 //Keybind Functions
 
 function keyDown (evt) {
